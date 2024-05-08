96435ebebf8c343aef0285de95e45eb28ca688f2
adlen <adlenfebi@gmail.com>
2024-05-08 13:54:15 +0530

    crew_mob no type

--- app/models/crew.model.js
+++ app/models/crew.model.js
@@ -47,7 +47,7 @@ module.exports = (sequelize, Sequelize) => {
       allowNull: true,
     },
     mobile_no: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.STRING,
       allowNull: true,
     },
     gender: {
