d7469cc1643032e1bed85c7e26bf84b1007eed4e
adlen <adlenfebi@gmail.com>
2024-05-09 12:18:04 +0530

    date changes

--- app/controllers/expense.controller.js
+++ app/controllers/expense.controller.js
@@ -13,9 +13,9 @@ const { StatusCode } = require("../constants/HttpStatusCode");
 exports.createExpense = async (req, res) => {
   try {
     // Ensure date_of_shooting is provided
-    if (!req.body.date_of_shooting) {
-      throw new Error("Date of shooting is required");
-    }
+    // if (!req.body.date_of_shooting) {
+    //   throw new Error("Date of shooting is required");
+    // }
 
     // Create expense
     const expenseData = {
@@ -36,7 +36,9 @@ exports.createExpense = async (req, res) => {
     const shootingData = {
       movie_id: req.body.movie_id,
       spot_id: req.body.spot_id,
-      date_of_shooting: req.body.date_of_shooting,
+      // category_id:req.body.category_id,
+      // sub_category_id: req.body.sub_category_id,
+      // crew_id:req.bod
     };
     const scheduleEntry = await movieschedule.create(shootingData);
 
@@ -56,15 +58,17 @@ exports.createExpense = async (req, res) => {
 exports.getExpenseDetails = async (req, res) => {
   try {
     const query = `
-    SELECT e.expense_id,m.movie_name,s.location,sch.date_of_shooting,c.crew_name,cat.category_name,sub.sub_category_name,e.no_of_staffs,e.advance_amount,e.beta FROM expensetracker_t_expense_t as e 
-left join expensetracker_t_movie_m as m on e.movie_id=m.movie_id 
-left join expensetracker_t_shootingspot_m as s on e.spot_id = s.spot_id
-left join expensetracker_t_movieschedule_t as sch on e.spot_id=sch.spot_id
-left join expensetracker_t_crew_m as c on e.crew_id = c.crew_id
-left join expensetracker_t_category_m as cat on e.category_id=cat.category_id
-left join expensetracker_t_subcategory_m as sub on e.sub_category_id=sub.sub_category_id
-WHERE e.active_status = 1 AND e.delete_status=0
-GROUP by expense_id;
+   SELECT e.expense_id, m.movie_name, DATE_FORMAT(e.date_of_shooting, '%d %b %Y') AS Date, s.location, c.crew_name, cat.category_name, sub.sub_category_name, e.no_of_staffs, e.advance_amount, e.beta 
+FROM expensetracker_t_expense_t AS e
+LEFT JOIN expensetracker_t_movie_m AS m ON e.movie_id = m.movie_id 
+LEFT JOIN expensetracker_t_shootingspot_m AS s ON e.spot_id = s.spot_id
+LEFT JOIN expensetracker_t_movieschedule_t AS sch ON e.spot_id = sch.spot_id
+LEFT JOIN expensetracker_t_crew_m AS c ON e.crew_id = c.crew_id
+LEFT JOIN expensetracker_t_category_m AS cat ON e.category_id = cat.category_id
+LEFT JOIN expensetracker_t_subcategory_m AS sub ON e.sub_category_id = sub.sub_category_id
+WHERE e.active_status = 1 AND e.delete_status = 0
+GROUP BY e.expense_id;
+
   `;
 
     const response = await db.sequelize.query(query, {
@@ -85,7 +89,7 @@ exports.getExpenseById = async (req, res) => {
   try {
     const expense_id = req.params.expense_id;
     const query = `
-    SELECT e.expense_id,m.movie_id,s.spot_id,sch.date_of_shooting,c.crew_name,cat.category_name,sub.sub_category_name,e.no_of_staffs,e.advance_amount,e.beta FROM expensetracker_t_expense_t as e 
+    SELECT e.expense_id,m.movie_id,s.spot_id,DATE_FORMAT(e.date_of_shooting, '%d %b %Y') AS Date,cat.category_name,sub.sub_category_name,e.no_of_staffs,e.advance_amount,e.beta FROM expensetracker_t_expense_t as e 
     left join expensetracker_t_movie_m as m on e.movie_id=m.movie_id 
     left join expensetracker_t_shootingspot_m as s on e.spot_id = s.spot_id
     left join expensetracker_t_movieschedule_t as sch on e.spot_id=sch.spot_id
